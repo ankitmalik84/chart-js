be2d7623a72f838cc6bc9b7c5ddbfdbfa5f22bad
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-07-09 13:13:57 +0530

    chart_practice

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode basename="https://github.com/ankitmalik84/chart-js/blob/master/src/index.js">
+  <React.StrictMode >
     <App />
   </React.StrictMode>
 );
